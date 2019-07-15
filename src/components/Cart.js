78cacb2d9655625c1c83817ce17054f1c92870a8
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-07-15 14:03:58 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import '../style.css'
 import {connect} from 'react-redux';
-import {hideCart} from '../actions'
-class Cart extends React.Component{
+import {hideCart , RemoveCart} from '../actions'
+class Cart extends React.Component{ 
+    subtotal(){
+        let subtotal=0
+        this.props.cart.map( product =>{
+            subtotal=subtotal+eval(product.price.slice(1))*product.quantity
+        })
+        return subtotal;
+    }
+    renderList(){
+        return this.props.cart.map( (product) =>{
+            var p= eval(product.price.slice(1))*product.quantity
+           return <div className="cartList">
+                        <div className="cartImgDiv"><img src={product.link} alt={product.name}/></div>
+                        <div className="cartDesc">
+                            <div>{product.name}</div>
+                            <div>{product.quantity}</div>
+                            <div>{product.price}</div>
+                        </div>
+                        <div className="proPrice"> ${p} </div>
+                        <div className="removePro" onClick={ ()=> this.props.RemoveCart(product) }>X</div>
+                  </div>
+        } )
+    }
     render(){
         return (
-           <div>
+           <div  id="cart">
                <div id="removeCart" onClick={()=>this.props.hideCart()}> X</div>
-               <div id="cart"></div>
+               <div id="mainCart"> {this.renderList()} </div>
+               <div id="checkout"> 
+                    <div id="price">
+                        <div id="subtotal">
+                            Subtotal
+                        </div>
+                        <div id="subprice">${this.subtotal()}</div>
+                    </div>
+                    <div id="check">CHECKOUT</div> 
+                </div> 
            </div> 
             
         )
@@ -15,7 +46,13 @@ class Cart extends React.Component{
 } 
 const mapDispatchToProps=(dispatch)=>{
     return {
-       hideCart: ()=>dispatch(hideCart())
+       hideCart: ()=>dispatch(hideCart()),
+       RemoveCart:(product)=>dispatch(RemoveCart(product))
     }
 }
-export default connect(null,mapDispatchToProps)(Cart);
+const mapStateToProps=(state)=>{
+    return {
+        cart:state.cartList,
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Cart);
