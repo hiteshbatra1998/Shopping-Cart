accd3c6e658572324c04b1b05d6d47853f777ad9
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-07-18 16:08:07 +0530

    Create ProductDescription.js

---
+++ src/components/ProductDescription.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Slider from './Slider'
+class ProductDescription extends React.Component{
+    render(){
+        return(
+            <div id="productModal"><Slider/></div>
+        )
+    }
+}
+export default ProductDescription
