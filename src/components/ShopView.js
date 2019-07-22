a8cbcf22db26332c3e2673ee6e0296f5e0e38129
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-07-22 12:23:13 +0530

    Update ShopView.js

--- src/components/ShopView.js
+++ src/components/ShopView.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../style.css';
 import {connect} from 'react-redux';
-import {AddCart , RemoveCart} from '../actions';
+import {showDescription , showPopup} from '../actions';
 class ShopView extends React.Component{
-    
+    // abc(pro){
+    //     this.props.showPop();
+    //     this.props.AddCart(pro);
+    // }
     renderList(){
         return this.props.list.shopList.map( (product)=>{
             return <div className="col-md-3 col-sm-4 col-12 " key={product.id}>
                         <div className="productView">
                             <div className="pro-image-div"> <img className="pro-image" src={product.link} alt={product.name} /></div>
-                            <div className="productName">{product.name}</div>
-                            <div>{product.price}</div>
-                            <div className="addCart"><div onClick={()=>this.props.AddCart(product)} >Add to cart</div></div>
+                            <div className="productName bounce">{product.name}</div>
+                            <div><b>{product.price}</b></div>
+                            <div className="addCart"><div onClick={ ()=>this.props.showDesc(product) } >Add to cart</div></div>
                             
                         </div>
                     </div>
@@ -29,8 +32,10 @@ const mapStateToProps=(state)=>{
 }
 const mapDispatchToProps=(dispatch)=>{
     return {
-        AddCart: product=>dispatch( AddCart(product)),
-        RemoveCart: product=>dispatch( RemoveCart(product) )
+        // AddCart: product=>dispatch( AddCart(product)),
+        // RemoveCart: product=>dispatch( RemoveCart(product) ),
+        showPop:()=>dispatch( showPopup() ),
+        showDesc:product=>dispatch( showDescription(product) )
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(ShopView);
