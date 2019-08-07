768403eebc04d9fc650b8b6b7d0766a2d87b0cb7
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-08-07 19:11:06 +0530

    Update SizeQuantity.js

--- src/components/SizeQuantity.js
+++ src/components/SizeQuantity.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Dropdown from 'react-dropdown'
 import {connect} from 'react-redux'
+import {AddProduct , showPopup, hidePopup} from '../actions'
 import 'react-dropdown/style.css'
 class SizeQuantity extends React.Component{
+    
+    changeQuant=(event)=>{
+        this.props.currentlySelected.quantity=event.value
+    }
+    changeSize=(event)=>{
+        this.props.currentlySelected.sizeSelected=event.value
+    }
     render(){
+        
+        if( this.props.add){
+            this.props.addToCart(this.props.currentlySelected)
+            this.props.showPop()
+            setTimeout( ()=>{this.props.hidePop()},1500 )
+        }
         const options = []
         for( let i=1;i<this.props.max;i++){
             options.push(i)
@@ -11,9 +25,8 @@ class SizeQuantity extends React.Component{
         let sizeAvailable=[]
         let flag=0
         for(let i=0;i<this.props.size.length;i++){
-            if(this.props.size[i]!=' '){
+            if(this.props.size[i]!==' '){
                 if(flag===0){
-            
                     sizeAvailable.push(this.props.size[i])
                     flag=1;
                 }
@@ -23,7 +36,6 @@ class SizeQuantity extends React.Component{
                 }
             }
             if(this.props.size[i]===' '){
-                
                 flag=0;
             }
         }
@@ -31,10 +43,10 @@ class SizeQuantity extends React.Component{
          <div id="main-size-add-div">   
             <div id="size-quant-div">
                 <div className="dropdown-quantity">
-                    <Dropdown options={options} onChange={this._onSelect}  placeholder="1" />
+                    <Dropdown id="dropdown" options={options} onChange={this.changeQuant}  placeholder="Select Quantity" />
                 </div>
                 <div className="dropdown-size">
-                    <Dropdown options={sizeAvailable} onChange={this._onSelect}  placeholder="Select Size" />
+                    <Dropdown options={sizeAvailable} onChange={this.changeSize}  placeholder="Select Size" />
                 </div>
             </div>
            
@@ -45,7 +57,16 @@ class SizeQuantity extends React.Component{
 const mapStateToProps=(state)=>{
     return {
         max:state.showProduct.atmax,
-        size:state.showProduct.size
+        size:state.showProduct.size,
+        currentlySelected:state.showProduct,
+        add:state.addCart
     }
 }
-export default connect(mapStateToProps)(SizeQuantity)
+const mapDispatchToProps=dispatch=>{
+    return{
+        addToCart:product=>dispatch( AddProduct(product) ),
+        showPop:()=>dispatch( showPopup() ),
+        hidePop:()=>dispatch(hidePopup() )
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(SizeQuantity)
