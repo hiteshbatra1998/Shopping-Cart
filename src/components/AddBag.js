92a971f5151e85aafc158c9a0099c4815ac68515
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-08-07 19:10:41 +0530

    Update AddBag.js

--- src/components/AddBag.js
+++ src/components/AddBag.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import{connect} from 'react-redux'
-import {AddCart} from '../actions'
+import {AddCart,AddCartToggle} from '../actions'
 class AddBag extends React.Component{
+    hide(){
+        this.props.add()
+        setTimeout( ()=>{ this.props.changeAddSTate() } ,1000 )
+        
+    }
     render(){
         return(
-            <div id="add-bag">Add To Bag</div>
+            <div id="add-bag" onClick={()=>this.hide()}>Add To Bag</div>
         )
     }
 }
-const mapDispatchToProps=(dispatch)=>{
-    return{
-        addCart:product=>dispatch( AddCart(product) )
+const mapDispatchToProps=dispatch=>{
+    return {
+        add:()=>dispatch( AddCart() ),
+        changeAddSTate:()=>dispatch(AddCartToggle())
     }
 }
-export default connect(null,mapDispatchToProps)(AddBag)
+export default connect(null,mapDispatchToProps)(AddBag);
