7b35964e77cf00d703748a8530bebe99182eeda0
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-08-08 21:57:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,56 +4,48 @@ import ShowCart from './ShowCart';
 import Cart from './Cart';
 import ProductDescription from './ProductDescription';
 import {connect} from 'react-redux';
-let flag=0
 class App extends React.Component{
     
     showCart(){
-        
-        var x=document.getElementById('cart')
         if(this.props.showCart){
-            flag=1
-            return <div><Cart /></div>
+            return<Cart />
         }
         else{
-            if(flag==1)
-                setTimeout( ()=>{ } ,1000)
-                
-            return <div><ShowCart /></div>
-            
+            return <ShowCart />
         }
     }
     showDesc(){
         if(this.props.showDesc){
             var x=document.getElementById('main-body')
-            var y=document.getElementsByClassName('addCart')
-            var z=document.getElementsByClassName('pro-image')
+            // var y=document.getElementsByClassName('addCart')
+            // var z=document.getElementsByClassName('pro-image')
             var u=document.getElementById('productModal');
             if(u!=null){    u.classList.add('productModalOpening')
             u.classList.remove('productModalClosing')}
             x.classList.add('bodyOpacity')
-            for(let i=0;i<y.length;i++){
-                y[i].classList.add('modalAnimate')
-            }
-            for(let i=0;i<z.length;i++){
-                z[i].classList.add('modalAnimate')
-            }
+            // for(let i=0;i<y.length;i++){
+            //     y[i].classList.add('modalAnimate')
+            // }
+            // for(let i=0;i<z.length;i++){
+            //     z[i].classList.add('modalAnimate')
+            // }
             return <ProductDescription/>
         }
         else{
-            var x=document.getElementById('main-body')
-            var y=document.getElementsByClassName('addCart')
-            var z=document.getElementsByClassName('pro-image')
+             x=document.getElementById('main-body')
+            // var y=document.getElementsByClassName('addCart')
+            // var z=document.getElementsByClassName('pro-image')
             if ( x!=null){    
                 x.classList.remove('bodyOpacity')
-                for(let i=0;i<y.length;i++){
-                    y[i].classList.remove('modalAnimate')
-                    y[i].classList.add('modalAnimateClose')
-                }
-                for(let i=0;i<z.length;i++){
-                    z[i].classList.remove('modalAnimate')
-                }
+                // for(let i=0;i<y.length;i++){
+                //     y[i].classList.remove('modalAnimate')
+                //     y[i].classList.add('modalAnimateClose')
+                // }
+                // for(let i=0;i<z.length;i++){
+                //     z[i].classList.remove('modalAnimate')
+                // }
             }
-            var u=document.getElementById('productModal');
+             u=document.getElementById('productModal');
             if(u!=null){
                 u.classList.remove('productModalOpening')
                 u.classList.add('productModalClosing')
@@ -64,8 +56,9 @@ class App extends React.Component{
     }
     render(){
         return <div id="main-body">
-                    <div ><ShopView /> 
-                    {this.showCart()}
+                    <div >
+                        <ShopView /> 
+                        {this.showCart()}
                     </div>
                    {this.showDesc()}
                 
